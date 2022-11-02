09544749b67fb659678a51ef8763bce8d81dca81
henriquemattia <mattia.henrique@gmail.com>
2022-11-02 15:09:06 -0300

    test3

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 import app from "./src/app.js";
 
-// const port = 3000;
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Servidor na porta ${port} iniciado com sucesso!`));
